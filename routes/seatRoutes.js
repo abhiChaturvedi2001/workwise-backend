bed5911a2cb8d1023548e4b3e3f876f6106dfb9b
ABHCH <chaturvedia435@gmail.com>
2024-12-30 12:17:23 +0530

    remove middleware

--- routes/seatRoutes.js
+++ routes/seatRoutes.js
@@ -1,10 +1,9 @@
 import express from "express"
 import { getAllSeats, reserveSeats, resetBooking } from "../controller/seatController.js";
-import { authValidation } from "../middleware/authMiddlware.js";
 const router = express.Router();
 
-router.post('/reserve', authValidation, reserveSeats);
+router.post('/reserve', reserveSeats);
 router.post('/reset', resetBooking);
-router.get('/', authValidation, getAllSeats);
+router.get('/', getAllSeats);
 
 export default router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,10 @@
 import express from "express"
 import { GetProfile, Login, Logout, Register } from "../controller/authController.js";
-import { authValidation } from "../middleware/authMiddlware.js";
 const router = express.Router();
 
 router.post("/register", Register);
 router.post("/login", Login);
 router.post("/logout", Logout);
-router.get("/profile", authValidation, GetProfile);
+router.get("/profile", GetProfile);
 
 export default router;
